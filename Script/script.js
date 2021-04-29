fd82522adce1652566527195e41f2ce97363aa0d
myweblinks <77198549+myweblinks@users.noreply.github.com>
2021-04-29 10:44:56 +0530

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -29,6 +29,67 @@ let latestContent6 = document.getElementById("latestContent6");
 let latestContent7 = document.getElementById("latestContent7");
 let latestContent8 = document.getElementById("latestContent8");
 let latestContent9 = document.getElementById("latestContent9");
+let itemCatefory1 = document.getElementById("itemCatefory1");
+let itemCatefory2 = document.getElementById("itemCatefory2");
+let itemCatefory3 = document.getElementById("itemCatefory3");
+let itemCatefory4 = document.getElementById("itemCatefory4");
+let itemCategory1 = document.getElementById("itemCategory1");
+let itemCategory2 = document.getElementById("itemCategory2");
+let itemCategory3 = document.getElementById("itemCategory3");
+let itemCategory4 = document.getElementById("itemCategory4");
+let itemCategory5 = document.getElementById("itemCategory5");
+let itemCategory6 = document.getElementById("itemCategory6");
+let itemCategory7 = document.getElementById("itemCategory7");
+let itemCategory8 = document.getElementById("itemCategory8");
+let itemCategory9 = document.getElementById("itemCategory9");
+let containsSoftwreByCategoryMainP = document.getElementById("containsSoftwreByCategoryMainP");
+let item1 = document.getElementById("item1");
+let item2 = document.getElementById("item2");
+let item3 = document.getElementById("item3");
+let item4 = document.getElementById("item4");
+let item5 = document.getElementById("item5");
+let item6 = document.getElementById("item6");
+let item7 = document.getElementById("item7");
+let item8 = document.getElementById("item8");
+let item9 = document.getElementById("item9");
+let item10 = document.getElementById("item10");
+let item11 = document.getElementById("item11");
+let item12 = document.getElementById("item12");
+let itemCategory10 = document.getElementById("itemCategory10");
+let itemCategory11 = document.getElementById("itemCategory11");
+let itemCategory12 = document.getElementById("itemCategory12");
+let itemCategory13 = document.getElementById("itemCategory13");
+let itemCategory14 = document.getElementById("itemCategory14");
+let itemCategory15 = document.getElementById("itemCategory15");
+let itemCategory16 = document.getElementById("itemCategory16");
+let itemCategory17 = document.getElementById("itemCategory17");
+let itemCategory18 = document.getElementById("itemCategory18");
+let itemCategory19 = document.getElementById("itemCategory19");
+let itemCategory20 = document.getElementById("itemCategory20");
+let itemCategory21 = document.getElementById("itemCategory21");
+let itemCategory22 = document.getElementById("itemCategory22");
+let itemCategory23 = document.getElementById("itemCategory23");
+let itemCategory24 = document.getElementById("itemCategory24");
+let itemCategory25 = document.getElementById("itemCategory25");
+let itemCategory26 = document.getElementById("itemCategory26");
+let itemCategory27 = document.getElementById("itemCategory27");
+let itemCategory28 = document.getElementById("itemCategory28");
+let itemCategory29 = document.getElementById("itemCategory29");
+let itemCategory30 = document.getElementById("itemCategory30");
+let itemCategory31 = document.getElementById("itemCategory31");
+let itemCategory32 = document.getElementById("itemCategory32");
+let itemCategory33 = document.getElementById("itemCategory33");
+let itemCategory34 = document.getElementById("itemCategory34");
+let itemCategory35 = document.getElementById("itemCategory35");
+let itemCategory36 = document.getElementById("itemCategory36");
+let itemCategory37 = document.getElementById("itemCategory37");
+let itemCategory38 = document.getElementById("itemCategory38");
+let itemCategory39 = document.getElementById("itemCategory39");
+let itemCategory40 = document.getElementById("itemCategory40");
+let itemCategory41 = document.getElementById("itemCategory41");
+let itemCategory42 = document.getElementById("itemCategory42");
+let itemCategory43 = document.getElementById("itemCategory43");
+let itemCategory44 = document.getElementById("itemCategory44");
 let WindowsGame1 = document.getElementById("WindowsGame1");
 let WindowsGame2 = document.getElementById("WindowsGame2");
 let WindowsGame3 = document.getElementById("WindowsGame3");
@@ -79,9 +140,12 @@ let previesColorTextBack = "#66ff0017";
 // For Loop In Array    
 let myobj1 = [LinkInNav1, LinkInNav2, LinkInNav3, LinkInNav4]
 let myobj2 = [DotsInThreeDotBtn1, DotsInThreeDotBtn2, DotsInThreeDotBtn3];
-let myobj3 = [darkmodeBtnMain, feedback,  otherSites, signInInNav, latestContent1, latestContent2, latestContent3, latestContent4, latestContent5, latestContent6, latestContent7, latestContent8, latestContent9,WindowsGame1, WindowsGame2, WindowsGame3, WindowsGame4, WindowsGame5, WindowsGame6, WindowsGame7, WindowsGame8, WindowsGame9];
+let myobj3 = [darkmodeBtnMain, feedback,  otherSites, signInInNav, latestContent1, latestContent2, latestContent3, latestContent4, latestContent5, latestContent6, latestContent7, latestContent8, latestContent9,WindowsGame1, WindowsGame2, WindowsGame3, WindowsGame4, WindowsGame5, WindowsGame6, WindowsGame7, WindowsGame8, WindowsGame9,itemCatefory1, itemCatefory2, itemCatefory3, itemCatefory4,itemCategory1, itemCategory2, itemCategory3, itemCategory4,itemCategory5 , itemCategory6 , itemCategory7 , itemCategory8 , itemCategory9 , itemCategory10, itemCategory11, itemCategory12, itemCategory13, itemCategory14, itemCategory15, itemCategory16, itemCategory17, itemCategory18, itemCategory19, itemCategory20, itemCategory21, itemCategory22, itemCategory23, itemCategory24, itemCategory25, itemCategory26, itemCategory27, itemCategory28, itemCategory29, itemCategory30, itemCategory31, itemCategory32, itemCategory33, itemCategory34, itemCategory35, itemCategory36, itemCategory37, itemCategory38, itemCategory39, itemCategory40, itemCategory41, itemCategory42, itemCategory43, itemCategory44, item1 , item2 , item3 , item4 , item5 , item6 , item7 , item8 , item9 , item10, item11, item12];
+// Some Variable Here
+
+
 // This Object is for white to lite black backgroud
-let myobj4 = [MainContainerOfLatestSoftware, MainContainerOfWindowsGames]; 
+let myobj4 = [MainContainerOfLatestSoftware, MainContainerOfWindowsGames, item1 , item2 , item3 , item4 , item5 , item6 , item7 , item8 , item9 , item10, item11, item12]; 
 
 
 
@@ -147,9 +211,6 @@ darkmodeBtnMain.addEventListener("click", function () {
     }
 });
 
-// Some Variable Here
-let containsSoftwreByCategoryMainP = document.getElementById("containsSoftwreByCategoryMainP");
-
 
 
 // Event With "For" Loop
@@ -159,31 +220,80 @@ for (let i = 0; i < MyObj.length; i++) {
             windowOfThreeDotBtn.style.display="none";
            }});
 }
-
-//Scroll Section
-// window.onscroll = function() {myFunction()};
-// function myFunction() {
-//     if (document.documentElement.scrollTop > 80) {
-//         mainNav.style.position="fixed";
-//         mainNav.style.marginTop="-1rem";
-        
-//     } else {
-//         mainNav.style.marginTop="0rem";
-//         mainNav.style.position="relative";
-        
-//     }
-// };
 window.onscroll = function() {myFunction()};
 function myFunction() {
     if (document.documentElement.scrollTop > 60) {
         MainContainerOfWindowsGames.className="AniArtiClass";
         containsSoftwreByCategoryMainP.className="AniArtiClass3ForP";
-        containsSoftwreByCategoryMainP.className="AniArtiClass4ForP";
         
         
     } else {
+        
         containsSoftwreByCategoryMainP.className="AniArtiClass3ForPReverse";
-        MainContainerOfWindowsGames.className="AniArtiClass2";
+        // MainContainerOfWindowsGames.className="AniArtiClass2";
+        
+    }
+    if (document.documentElement.scrollTop > 250) {
+        containerCategory1.className="AniArtiClass3ForP";
+        
+        item1.className="AniArtiClass4ForP";
+        item2.className="AniArtiClass5ForP";
+        item3.className="AniArtiClass6ForP";
+        
+        
+    } else {
+        containerCategory1.className="AniArtiClass3ForPReverse";
+        item1.className="AniArtiClass4rForPReverse";
+        item2.className="AniArtiClass5rForPReverse";
+        item3.className="AniArtiClass6rForPReverse";
+        
+    }
+    if (document.documentElement.scrollTop > 650) {
+        containerCategory2.className="AniArtiClass3ForP";
+        item4.className="AniArtiClass4ForP";
+        item5.className="AniArtiClass5ForP";
+        item6.className="AniArtiClass6ForP";
+        
+        
+    } else {
+        item4.className="AniArtiClass4rForPReverse";
+        item5.className="AniArtiClass5rForPReverse";
+        item6.className="AniArtiClass6rForPReverse";
+        
+        containerCategory2.className="AniArtiClass3ForPReverse";
+        // MainContainerOfWindowsGames.className="AniArtiClass2";
+        
+    }
+    if (document.documentElement.scrollTop > 950) {
+        containerCategory3.className="AniArtiClass3ForP";
+        item7.className="AniArtiClass4ForP";
+        item8.className="AniArtiClass5ForP";
+        item9.className="AniArtiClass6ForP";
+        
+        
+    } else {
+        item7.className="AniArtiClass4rForPReverse";
+        item8.className="AniArtiClass5rForPReverse";
+        item9.className="AniArtiClass6rForPReverse";
+        
+        containerCategory3.className="AniArtiClass3ForPReverse";
+        // MainContainerOfWindowsGames.className="AniArtiClass2";
+        
+    }
+    if (document.documentElement.scrollTop > 1250) {
+        containerCategory4.className="AniArtiClass3ForP";
+        item10.className="AniArtiClass4ForP";
+        item11.className="AniArtiClass5ForP";
+        item12.className="AniArtiClass6ForP";
+        
+        
+    } else {
+        item10.className="AniArtiClass4rForPReverse";
+        item11.className="AniArtiClass5rForPReverse";
+        item12.className="AniArtiClass6rForPReverse";
+        
+        containerCategory4.className="AniArtiClass3ForPReverse";
+        // MainContainerOfWindowsGames.className="AniArtiClass2";
         
     }
 };
